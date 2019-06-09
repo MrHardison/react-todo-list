c8e6446310fcf0ecfbc4dbb5a9d6aab8d3564204
Dmitriy <disscovery.993@gmail.com>
2019-06-09 17:22:41 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,44 @@ import AddTodo from './AddTodo/AddTodo.js'
 import Context from './Context'
 
 function App() {
-  const [list, setList] = React.useState([])
+  const [list, setList] = React.useState([
+    {
+      userId: 1,
+      id: 1,
+      title: 'delectus aut autem',
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'quis ut nam facilis et officia qui',
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: 'fugiat veniam minus',
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 4,
+      title: 'et porro tempora',
+      completed: true
+    },
+    {
+      userId: 1,
+      id: 5,
+      title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+      completed: false
+    }
+  ])
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=7')
-      .then(response => response.json())
-      .then(list => setList(list))
-  })
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos?_limit=7')
+  //     .then(response => response.json())
+  //     .then(list => setList(list))
+  // })
 
   function toggleItem(id) {
     setList(
